2b2d6c1dd221efacc37fce970683e1679d443c55
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-11 18:08:23 -0800

    Updated project structure

---
+++ src/api/inrix.py
@@ -0,0 +1,59 @@
+from urllib.parse import quote
+
+import requests
+
+
+class InrixAPI:
+    api_url: str
+
+    appid: str
+    hash_token: str
+    access_token: str
+
+    def __init__(self, appid: str, hash_token: str, api_url: str = "https://api.iq.inrix.com"):
+        self.appid = appid
+        self.hash_token = hash_token
+
+        self.api_url = api_url
+
+        self.update_token()
+
+    def update_token(self):
+        self.access_token = self.get("auth/v1/appToken", {
+            "appId": self.appid,
+            "hashToken": self.hash_token
+        }, use_auth=False).json()["result"]["token"]
+
+    def get_drivetime_polygons(self, center=None, range_type=None, duration=None, datetime=None, criteria=None):
+        param = {}
+
+        if center is not None:
+            param["center"] = center
+
+        if range_type is not None:
+            param["rangeType"] = range_type
+
+        if duration is not None:
+            param["duration"] = duration
+
+        if datetime is not None:
+            param["dateTime"] = datetime
+
+        if criteria is not None:
+            param["criteria"] = criteria
+
+        print(param)
+
+        return self.get("drivetimePolygons", param)
+
+    def get(self, endpoint: str, queries: dict[str, str] = {}, headers: dict[str, str] = {}, use_auth: bool = True):
+        # Generate the URL to call
+        call_url = f"{self.api_url}/{endpoint}"
+
+        # Add authentication to the header
+        if use_auth:
+            headers["Authorization"] = f"Bearer {self.access_token}"
+
+        print(requests.Request("GET", call_url, params=queries, headers=headers).prepare().url)
+
+        return requests.get(call_url, params=queries, headers=headers)
--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def index():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/process/<username>/")
+@app.route("/process/<string:request>/<int:start_time>/<int:duration>/<int:loc_lat>/<int:loc_lon>")
 def process():
     pass
 
