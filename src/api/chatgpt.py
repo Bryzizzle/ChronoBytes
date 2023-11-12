c6a810a46732291c2c3afada35ff22107575ca09
roblee <roblee04@gmail.com>
2023-11-11 20:17:06 -0800

    chatgpt

--- src/api/chatgpt.py
+++ src/api/chatgpt.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key="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",
+    api_key= os.getenv("OPENAI_KEY"),
 )
 
 chat_completion = client.chat.completions.create(
