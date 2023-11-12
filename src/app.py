99d9c3cbae94622c42477d9f0ef45a0493adb461
roblee <roblee04@gmail.com>
2023-11-11 20:42:38 -0800

    app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from os import getenv
+from api.inrix import InrixAPI
+from api.yelp import YelpAPI
+
+INRIX_APPID = os.getenv('INRIX_APPID')
+INRIX_TOKEN = os.getenv('INRIX_TOKEN')
+YELP_APIKEY = os.getenv('YELP_APIKEY')
+print(INRIX_APPID, INRIX_TOKEN, YELP_APIKEY)
+
+
 
 app = Flask(__name__)
 
@@ -9,7 +19,16 @@ def index():
 
 
 @app.route("/process/<string:request>/<int:start_time>/<int:duration>/<int:loc_lat>/<int:loc_lon>")
+
 def process():
+
+    req = request
+    # chat gpt to parse req
+
+    # location and time into inrix
+    inrix = InrixAPI()
+
+
     pass
 
 
